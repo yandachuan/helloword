eaff7eef0dbd35b56a9974083fd5d35738e26a55
1533619899 <1533619899@qq.com>
2020-12-15 09:50:35 +0800

    dubbo接口

---
+++ hello-api/src/main/java/com/hello/api/service/testUser.java
@@ -0,0 +1,27 @@
+package com.hello.api.service;
+
+
+import com.github.pagehelper.PageInfo;
+import com.hello.api.dto.UserDTO;
+import com.hello.api.req.HelloRequest;
+import com.hello.api.req.UserRequest;
+
+import java.util.List;
+
+
+public interface testUser {
+
+    String getResult(HelloRequest helloRequest);
+
+    Long insert(UserRequest entity);
+
+    Long deleteByUserId(Integer userId);
+
+    Long update(UserRequest entity);
+
+    UserDTO getByUserId(Integer userId);
+
+    List<UserDTO> findUserList();
+
+    PageInfo<UserDTO> getUserList(int pageNum, int pageSize);
+}
